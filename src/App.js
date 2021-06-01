dc0bd63a5feb1cd12ef37a88d57684491a8b63e5
Saman Manesh <samanmanesh7@gmail.com>
2021-06-01 19:42:50 -0400

    Start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import * as Tone from "tone";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    //attach a click listener to a play button
+    document.querySelector("button")?.addEventListener("click", async () => {
+      await Tone.start();
+      console.log("audio is ready");
+    });
+  }, []);
+  const play = () => {
+    // Tone.start();
+    //create a synth and connect it to the main output (your speakers)
+    const synth = new Tone.Synth().toDestination();
+    const now = Tone.now();
+    // synth.triggerAttackRelease("C4", "8n", now);
+    // synth.triggerAttackRelease("E4", "8n", now + 0.5);
+    // synth.triggerAttackRelease("G4", "8n", now + 1);
+
+    const notes = ["C4", "G4", "E4", "A4", "E4", "A4", "E4", "A4"]
+    let i = 0;
+    const loop = new Tone.Loop((time) => {
+      // triggered every eighth note.
+      console.log(time);
+      synth.triggerAttackRelease(notes[i], "8n");
+      i++;
+      if (i > notes.length - 1) i = 0;
+    }, "8n").start(0);
+    Tone.Transport.start();
+
+    //play a middle 'C' for the duration of an 8th note
+    // synth.triggerAttackRelease("C4", "8n");
+
+    // setInterval(() => console.log(Tone.now()), 100);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hi
+      <button onClick={() => play()}>Play</button>
     </div>
   );
 }

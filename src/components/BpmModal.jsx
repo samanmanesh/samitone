2984bde97787e08f31024bf469a208828942e955
Saman Manesh <samanmanesh7@gmail.com>
2021-06-18 19:08:17 -0400

    change the style of bpm /grid added

--- src/components/BpmModal.jsx
+++ src/components/BpmModal.jsx
@@ -19,63 +19,90 @@ const ModalWrapper = styled.div`
   z-index: 10;
   background: ${colors.background.primaryDark};
 
-  display: flex;
-  flex-direction: column;
+  display: grid;
+  grid-template-columns: auto;
+  grid-template-rows: 1rem 4rem 2rem 1rem;
+  grid-template-areas:
+    "header header header header"
+    "Left  main main  main "
+    "bar bar sig sig"
+    "nameBar nameBar nameSig nameSig";
 
-  .bpm {
-    /* background: #4b4b9b; */
-    height: 50%;
-    & > div {
-      width: 2rem;
-      /* margin-left: auto;
-        margin-right:9.5rem; */
-      margin: 0.5rem 9.5rem 0 auto;
-      font-size: 1.5ch;
-    }
-    & > span {
-      margin-left: 1rem;
-    }
-
-    .slider {
-      /* padding-bottom: .2rem; */
-      margin: 1.3rem 0 0 2.5rem;
-      -webkit-appearance: none; //Override default CSS styles
-      appearance: none;
-      width: 71%; /* Full-width */
-      height: 0.3rem; /* Specified height */
-      background: rgba(153, 199, 255, 0.79);
-      outline: none; /* Remove outline */
-      opacity: 1; /* Set transparency (for mouse-over effects on hover) */
-      -webkit-transition: 50s; /* 0.2 seconds transition on hover */
-      transition: opacity 0.2s;
-      border-radius: 0.1rem;
-    }
-
-    /* The slider handle (use -webkit- (Chrome, Opera, Safari, Edge) and -moz- (Firefox) to override default look) */
-    .slider::-webkit-slider-thumb {
-      -webkit-appearance: none; /* Override default look */
-      appearance: none;
-      width: 1.5rem; /* Set a specific slider handle width */
-      height: 1.5rem; /* Slider handle height */
-      background: rgba(69, 56, 214, 0.99);
-      cursor: pointer; /* Cursor on hover */
-      border-radius: 0.2rem;
-    }
-
-    .slider::-moz-range-thumb {
-      width: 2rem; /* Set a specific slider handle width */
-      height: 2rem; /* Slider handle height */
-      background: rgba(69, 56, 214, 0.99);
-      cursor: pointer; /* Cursor on hover */
-    }
+  .bpm-display {
+    grid-area: header;
+    justify-self: center;
+    align-self: center;
+    margin-top: 1rem;
   }
-  .bar-sig-container {
-    background: #632d2d;
-    height: 50%;
-    display: flex;
-    .bpm-select {
-        margin-left: 3rem;
-    }
+
+  & > span {
+    grid-area: Left;
+    justify-self: center;
+    align-self: center;
+  }
+
+  .slider {
+    grid-area: main;
+    justify-self: start;
+    align-self: center;
+    margin-left: 0.5rem;
+    -webkit-appearance: none; //Override default CSS styles
+    appearance: none;
+    width: 85%; /* Full-width */
+    height: 0.3rem; /* Specified height */
+    background: rgba(153, 199, 255, 0.79);
+    outline: none; /* Remove outline */
+    opacity: 1; /* Set transparency (for mouse-over effects on hover) */
+    -webkit-transition: 50s; /* 0.2 seconds transition on hover */
+    transition: opacity 0.2s;
+    border-radius: 0.1rem;
+  }
+
+  /* The slider handle (use -webkit- (Chrome, Opera, Safari, Edge) and -moz- (Firefox) to override default look) */
+  .slider::-webkit-slider-thumb {
+    grid-area: main;
+    justify-self: start;
+    align-self: center;
+    margin-left: 1rem;
+    -webkit-appearance: none; /* Override default look */
+    appearance: none;
+    width: 1.5rem; /* Set a specific slider handle width */
+    height: 1.5rem; /* Slider handle height */
+    background: rgba(69, 56, 214, 0.99);
+    cursor: pointer; /* Cursor on hover */
+    border-radius: 0.2rem;
+  }
+
+  .slider::-moz-range-thumb {
+    grid-area: main;
+    justify-self: start;
+    align-self: center;
+    margin-left: 1rem;
+    width: 2rem; /* Set a specific slider handle width */
+    height: 2rem; /* Slider handle height */
+    background: rgba(69, 56, 214, 0.99);
+    cursor: pointer; /* Cursor on hover */
+  }
+
+  .bpm-select {
+    grid-area: bar;
+    justify-self: end;
+  }
+
+  .time-sig-select {
+    grid-area: sig;
+    justify-self: center;
+  }
+
+  .name-time-sig {
+    grid-area: nameSig;
+    justify-self: center;
+  }
+
+  .name-bars {
+    grid-area: nameBar;
+    justify-self: end;
+    margin-right: 1rem;
   }
 `;
 
@@ -83,11 +110,9 @@ const ModalWrapper = styled.div`
 
 export default function BpmModal() {
   const { options, setOptions } = useSong();
-  //   const [bt1IsClick, setBt1IsClick] = useState(true);
-  //   const [bt2IsClick, setBt2IsClick] = useState(false);
-
   const [selectedBarsIndex, setSelectedBarsIndex] = useState(0);
   const [selectedTimeSigIndex, setSelectedTimeSigIndex] = useState(0);
+
   const bpmConverter = (e) => {
     const bpm = e.target.value;
     const hz = bpm / 60;
@@ -96,100 +121,38 @@ export default function BpmModal() {
     setOptions({ ...options, bps: hz });
   };
 
-  //   const barsButtonsHandler = (e) => {
-  //     if (e === bt1IsClick) {
-  //       setBt1IsClick((prev) => !prev);
-  //       setBt2IsClick((prev) => !prev);
-  //     }
-  //     if (e === bt2IsClick) {
-  //       setBt2IsClick((prev) => !prev);
-  //       setBt1IsClick((prev) => !prev);
-  //     }
-  //   };
-
-  //   console.log(options.bps * 60);
-  //   console.log(bt1IsClick, "bt1IsClick");
-  //   console.log(bt2IsClick, "bt2IsClick");
   return (
     <ModalWrapper>
-      <section className="bpm">
-        <div>{Math.round(options.bps * 60)}</div>
+      <div className="bpm-display">{Math.round(options.bps * 60)}</div>
 
-        <span>BPM</span>
+      <span>BPM</span>
 
-        <input
-          type="range"
-          min="70"
-          max="150"
-          className="slider"
-          // value={bps}
-          value={options.bps * 60}
-          // onChange={(e) => setBps(e.target.value)}
-          onChange={(e) => bpmConverter(e)}
-        />
-      </section>
-
-      <section className="bar-sig-container">
-        {/* <BarButtons /> */}
-        <SelectInput
-          options={["4/4", "8/4"]}
-          selectedIndex={selectedBarsIndex}
-          setSelectedIndex={setSelectedBarsIndex}
-          className="bpm-select"
-        />
-
-        {/* <div className="bar">
-          <div className="bar-buttons-container">
-            <button
-              className="btn-1"
-              style={
-                bt1IsClick
-                  ? { background: "rgb(101, 96, 255)" }
-                  : { background: "rgb(55, 52, 52)" }
-              }
-              onClick={() => barsButtonsHandler(bt1IsClick)}
-              value={bt1IsClick}
-            >
-              4/4
-            </button>
-            <button
-              className="btn-2"
-              style={
-                bt2IsClick
-                  ? { background: "rgb(101, 96, 255)" }
-                  : { background: "rgb(55, 52, 52)" }
-              }
-              onClick={() => barsButtonsHandler(bt2IsClick)}
-            >
-              8/8
-            </button>
-          </div>
-          <p>BARS</p>
-        </div> */}
-        {/* <TimeSigButtons/> */}
-        {/* <div className="sig">
-          <div className="sig-buttons-container">
-
-              <button className="btn-3"></button>
-              <button className="btn-4"></button>
-              <button className="btn-5"></button>
-              <button className ="btn-6"></button>
-          </div>
-        </div> */}
-        <SelectInput
-          options={["4", "8", "16", "24"]}
-          selectedIndex={selectedTimeSigIndex}
-          setSelectedIndex={setSelectedTimeSigIndex}
-        />
-      </section>
-
-      {/* <input
-        type="number"
+      <input
+        type="range"
+        min="70"
+        max="150"
+        className="slider"
         // value={bps}
-        value={options.bps}
+        value={options.bps * 60}
         // onChange={(e) => setBps(e.target.value)}
-        onChange={(e) => setOptions({ ...options, bps: e.target.value })}
-      /> */}
+        onChange={(e) => bpmConverter(e)}
+      />
+
+      <SelectInput
+        options={["4/4", "8/4"]}
+        selectedIndex={selectedBarsIndex}
+        setSelectedIndex={setSelectedBarsIndex}
+        className="bpm-select"
+      />
+      <p className="name-time-sig">TIME SIG</p>
+
+      <SelectInput
+        options={["4", "8", "16", "24"]}
+        selectedIndex={selectedTimeSigIndex}
+        setSelectedIndex={setSelectedTimeSigIndex}
+        className="time-sig-select"
+      />
+      <p className="name-bars">BARS</p>
     </ModalWrapper>
   );
 }

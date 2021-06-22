21e4a84d4dca9b94bdbeef21ce46eb2028b82893
Saman Manesh <samanmanesh7@gmail.com>
2021-06-22 14:32:12 -0400

    cleaned up comments on trackRow

--- src/components/TrackRow.jsx
+++ src/components/TrackRow.jsx
@@ -14,9 +14,7 @@ import useSong from "../helpers/useSong";
 // #region - Styling -
 
 const TrackRowWrapper = styled.div`
-  /* border: 1px solid white; */
   border-radius: 0.25rem;
-  /* padding: 1rem; */
   margin-bottom: 0.5rem;
   position: relative;
   z-index: 2;
@@ -24,30 +22,16 @@ const TrackRowWrapper = styled.div`
   .controller-notes-container {
     display: flex;
     justify-content: space-between;
-    /* background: #376b81; */
     & > div {
-      /* margin: 1rem; */
     }
   }
-  /* .effects-volume-container {
-    display: flex;
-    justify-content: space-between;
-    background: #357e56;
-    & > div {
-      margin: 1rem;
-    }
-  } */
+
 `;
 
 const NotesWrapper = styled.div`
   width: 100%;
-  /* background: #624848; */
   display: flex;
   flex-direction: column;
-  /* justify-content: space-evenly; */
-  /* justify-content:center; */
-  /* height:100%; */
-  /* flex-grow:10; */
 `;
 
 
@@ -57,15 +41,7 @@ const Notes = styled.div`
   z-index: 0;
   display: flex;
   padding: 0 0 0 1rem;
-  /* flex-direction: column; */
-  /* justify-content: space-evenly; */
-  /* align-content: space-between;  */
-  /* height:100%; */
-  /* min-height:.5rem; */
-  /* padding: 1rem; */
 
-  /* border-radius: 4rem; */
-  /* margin-bottom: 0.5rem; */
   & > * {
     margin-right: 0.5rem;
 
@@ -86,23 +62,16 @@ export default function TrackRow({ track, currentStep }) {
 
   const [isCollapsed, setIsCollapsed] = useState(false);
   const changedNote = (note) => {
-    // console.log("changed note", note);
 
     const updatedNotes = track.notes;
     updatedNotes[note.row][note.order].active =
       !updatedNotes[note.row][note.order].active;
 
-    // console.log("calling update track", updatedNotes);
     updateTrack(track.id, { ...track, notes: updatedNotes });
   };
 
   const colors = getInstrument(track.instrument).colors;
 
-  // const filterHandler = (e) => {
-  //   const newOptions = track.options;
-  //   newOptions.filter = e.target.value;
-  //   updateTrack(track.id, { ...track, options: newOptions });
-  // };
 
   return (
     <TrackRowWrapper>
@@ -131,22 +100,7 @@ export default function TrackRow({ track, currentStep }) {
                       )
                   )}
                   
-                {/* {row
-                .filter(
-                  (note) =>
-                    note.order < options.stepLength &&
-                    note.row < track.rowDisplay
-                  // note.order < options.stepLength
-                  )
-                  .map((note) => (
-                    <Note
-                      note={note}
-                      toggleNote={() => changedNote(note)}
-                      currentStep={currentStep}
-                      key={uuidv4()}
-                      colors={colors}
-                    />
-                  ))} */}
+               
               </Notes>
             ))}
 
@@ -171,12 +125,7 @@ export default function TrackRow({ track, currentStep }) {
         </NotesWrapper>
       </section>
 
-      {/* <section className="effects-volume-container">
-        <div>effect
-          <input type="number" value={track.options.filter} onChange={filterHandler} />
-        </div>
-        <div>volume section</div>
-      </section> */}
+      
     </TrackRowWrapper>
   );
 }

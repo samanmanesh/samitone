dda55ed0df49e42a52f5436590439260e826a94c
Saman Manesh <samanmanesh7@gmail.com>
2021-07-20 18:44:28 -0400

    cleaned up instruments.js

--- src/helpers/instruments.js
+++ src/helpers/instruments.js
@@ -18,40 +18,6 @@ export const getInstrumentsByType = (instrumentType) =>
   instruments.filter((instrument) => instrument.type === instrumentType);
 
 export const instruments = [
-  // {
-  //   name: "FM",
-  //   sound: new Tone.FMSynth(),
-  //   octave: 5,
-  //   type: InstrumentType.Synth,
-  //   colors: colors.pink,
-  // },
-  // {
-  //   name: "AM",
-  //   sound: new Tone.AMSynth(),
-  //   octave: 3,
-  //   type: InstrumentType.Synth,
-  //   colors: colors.orange,
-  // },
-  // {
-  //   name: "Duo",
-  //   sound: new Tone.DuoSynth(),
-  //   octave: 4,
-  //   type: InstrumentType.Synth,
-  //   colors: colors.orange,
-  // },
-  // {
-  //   name: "Sample",
-  //   sound: new Tone.Sampler({
-  //     urls: {
-  //       A1: "A1.mp3",
-  //       A2: "A2.mp3",
-  //     },
-  //     baseUrl: "https://tonejs.github.io/audio/casio/",
-  //   }),
-  //   octave: 5,
-  //   type: InstrumentType.Synth,
-  //   colors: colors.teal,
-  // },
   {
     name: "Kick",
     sound: new Tone.Sampler({
@@ -593,5 +559,3 @@ export const instruments = [
     colors: colors.pink,
   },
 ];
-
-
